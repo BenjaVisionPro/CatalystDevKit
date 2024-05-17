5dab4abef015960a21167c689002ef0f7290a064
Jupiter Jones <jupiter.jones@mail.com>
2024-05-18 04:08:08 +1000

    WIP - dev kit installer

---
+++ bin/setupGTDev.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env sh
+PROGNAME=$0
+. private/shFeedback
+start_banner
+
+# todo: download for the right platform
+downloadFile=`curl -s -o /dev/stdout https://dl.feenk.com/gt/GlamorousToolkitOSXM1-release`
+downloadLink=https://dl.feenk.com/gt/${downloadFile}
+
+# install location
+installLocation=$(PWD)/gt
+
+information_banner "Creating Install Location: ${installLocation}"
+# create install location
+mkdir -p $installLocation
+
+# download and unzip
+information_banner "Downloading from: ${downloadLink}"
+curl -LO "${downloadLink:-}" \
+&& unzip -qq -d "${installLocation}" "${downloadFile}" \
+&& rm "${downloadFile}"
+
+# install stuff in gt
+information_banner "Starting installation"
+spinner_start "Installing Projects... "
+$installLocation/GlamorousToolkit.app/Contents/MacOS/GlamorousToolkit-cli $installLocation/GlamorousToolkit.image st "loadBVCDevKit.st" --interactive --save --quit
+spinner_stop
